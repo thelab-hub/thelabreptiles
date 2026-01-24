6117e62f91e6f622d079679e1935bad28bf210f3
thelab-hub <thelab@thelabreptiles.com>
2026-01-24 12:40:38 -0500

    updating collection system

---
+++ collection.js
@@ -0,0 +1,38 @@
+const collection = [
+    {
+        id: "Demeter",
+        name: "Demeter",
+        morph: "Bacon Quad Stripe Gargoyle",
+        badge: "Bacon Line",
+        status: "CURRENTLY BREEDING",
+        statusColor: "#00ff00", // Green
+        sex: "Female",
+        lineage: "Foundational Bacon Line",
+        description: "Demeter is a powerhouse within our Gargoyle project. Her saturation and structural cresting are hallmarks of the Bacon lineage.",
+        photos: ["ReptilePhotos/Demeter1.jpg", "ReptilePhotos/Demeter2.jpg", "ReptilePhotos/Demeter3.jpg"]
+    },
+    {
+        id: "Artemis",
+        name: "Artemis",
+        morph: "Red Bicolour Crested Gecko",
+        badge: "Red Line",
+        status: "BREEDING 2027",
+        statusColor: "var(--bacon-orange)", 
+        sex: "Female",
+        lineage: "Venus x Eros",
+        description: "Artemis represents our future in high-saturation Reds. She is currently being grown out for our 2027 season.",
+        photos: ["ReptilePhotos/Artemis1.jpg", "ReptilePhotos/Artemis2.jpg", "ReptilePhotos/Artemis3.jpg"]
+    },
+    {
+        id: "Kybele",
+        name: "Kybele",
+        morph: "Deep Red Lily White",
+        badge: "Lily White",
+        status: "BREEDING 2027",
+        statusColor: "var(--bacon-orange)",
+        sex: "Female",
+        lineage: "Red Lineage",
+        description: "Kybele is a project-defining female, combining the cleanliness of the Lily White trait with deep, velvet reds.",
+        photos: ["ReptilePhotos/Kybele1.jpg", "ReptilePhotos/Kybele2.jpg", "ReptilePhotos/Kybele3.jpg"]
+    }
+];
