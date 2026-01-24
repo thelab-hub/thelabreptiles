29776f7854818b3a3199f38eeea9885ca180aaa9
thelab-hub <thelab@thelabreptiles.com>
2026-01-24 12:21:46 -0500

    SEO

---
+++ inventory.js
@@ -0,0 +1,58 @@
+const inventory = [
+    {
+        id: "GG-ATH-03-2-25",
+        name: "Bacon Quad Stripe",
+        price: "$225",
+        weight: "10g",
+        hatch: "July 17, 2025",
+        link: "GG-ATH-03-2-25.html",
+        sold: false,
+        photos: [
+            "ReptilePhotos/GG-ATH-03-2-25-1.jpg",
+            "ReptilePhotos/GG-ATH-03-2-25-2.jpg",
+            "ReptilePhotos/GG-ATH-03-2-25-3.jpg"
+        ]
+    },
+    {
+        id: "GG-ATH-03-1-25",
+        name: "Bacon Quad Stripe",
+        price: "$350",
+        weight: "10g",
+        hatch: "July 14, 2025",
+        link: "GG-ATH-03-1-25.html",
+        sold: false,
+        photos: [
+            "ReptilePhotos/GG-ATH-03-1-25-1.jpg",
+            "ReptilePhotos/GG-ATH-03-1-25-2.jpg",
+            "ReptilePhotos/GG-ATH-03-1-25-3.jpg"
+        ]
+    },
+    {
+        id: "GG-VE-01-1-24",
+        name: "Orange Stripe White Base",
+        price: "$200",
+        weight: "16g",
+        hatch: "July 21, 2024",
+        link: "GG-VE-01-1-24.html",
+        sold: false,
+        photos: [
+            "ReptilePhotos/GG-VE-01-01-24-1.jpg",
+            "ReptilePhotos/GG-VE-01-01-24-2.jpg",
+            "ReptilePhotos/GG-VE-01-01-24-3.jpg"
+        ]
+    },
+    {
+        id: "GG-HES-03-1-24",
+        name: "Bacon Quad Stripe",
+        price: "$400",
+        weight: "13g",
+        hatch: "Sept 15, 2024",
+        link: "GG-HES-03-1-24.html",
+        sold: false,
+        photos: [
+            "ReptilePhotos/GG-HES-03-1-24-1.jpg",
+            "ReptilePhotos/GG-HES-03-1-24-2.jpg",
+            "ReptilePhotos/GG-HES-03-1-24-3.jpg"
+        ]
+    }
+];
